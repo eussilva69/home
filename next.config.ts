4943e41cb0a842330e5cebfcde3b19f1386a82c4
Gabriel Silva <gabrieltu134@gmail.com>
2025-08-03 22:24:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -159,6 +159,12 @@ const nextConfig: NextConfig = {
         hostname: 'http2.mlstatic.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.cdnandroid.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },

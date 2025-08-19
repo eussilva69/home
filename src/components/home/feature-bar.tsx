55f3f20f06e1dc4cc001a7e0f5db9bfdd25ff331
Gabriel Silva <gabrieltu134@gmail.com>
2025-08-19 23:54:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <svg xmlns="http:/

--- src/components/home/feature-bar.tsx
+++ src/components/home/feature-bar.tsx
@@ -1,5 +1,5 @@
 
-import { Brush, CreditCard, Shield, Truck } from "lucide-react"
+import { Brush, CreditCard, Truck } from "lucide-react"
 
 const features = [
   {
@@ -13,7 +13,7 @@ const features = [
     description: "até 12x sem juros",
   },
   {
-    icon: <Shield className="h-8 w-8 text-primary" />,
+    icon: <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="h-8 w-8 text-primary"><title>AdGuard</title><path d="M12 0C8.249 0 3.725.861 0 2.755 0 6.845-.051 17.037 12 24 24.051 17.037 24 6.845 24 2.755 20.275.861 15.751 0 12 0zm-.106 15.429L6.857 9.612c.331-.239 1.75-1.143 2.794.042l2.187 2.588c.009-.001 5.801-5.948 5.815-5.938.246-.22.694-.503 1.204-.101l-6.963 9.226z"/></svg>,
     title: "Compra Segura",
     description: "dados protegidos",
   },
